0a81a4b140fe21d8f4aa4f957afe9c21d8242277
shamil16 <chernovshamil6@gmail.com>
2022-12-24 20:48:32 +0600

    Initial commit

---
+++ app/src/main/java/com/example/lesson6_1hw/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.lesson6_1hw
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.lesson6_1hw.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initClick()
+    }
+
+    private fun initClick() {
+        val launcherData =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK && result.data != null) {
+                    val data =
+                        result.data?.getStringExtra(SecondActivity.KEYFORDATA)
+                    binding.et.setText(data)
+                }
+            }
+
+        binding.btn.setOnClickListener {
+            if (binding.et.text.isEmpty()) {
+                Toast.makeText(this , R.string.toast , Toast.LENGTH_SHORT).show()
+            }else {
+                Intent(this@MainActivity , SecondActivity::class.java).apply {
+                    putExtra(KEYFORINTENT , binding.et.text.toString())
+                    launcherData.launch(this)
+                }
+            }
+        }
+    }
+
+    companion object {
+        const val KEYFORINTENT = "DataKey"
+    }
+
+
+}
---
+++ app/src/main/java/com/example/lesson6_1hw/SecondActivity.kt
@@ -0,0 +1,42 @@
+package com.example.lesson6_1hw
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.lesson6_1hw.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySecondBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initListener()
+    }
+
+    private fun initListener() {
+        val desc = intent.getStringExtra(MainActivity.KEYFORINTENT)
+        binding.et.setText(desc)
+
+        binding.btn.setOnClickListener {
+            val data = binding.et.text.trim().toString()
+
+            if (binding.et.text.isEmpty()) {
+                Toast.makeText(this , R.string.toast , Toast.LENGTH_SHORT).show()
+            }else {
+                Intent(this@SecondActivity , MainActivity::class.java).apply {
+                    putExtra(KEYFORDATA , data)
+                    setResult(RESULT_OK , this)
+                    finish()
+                }
+            }
+        }
+    }
+
+    companion object {
+        const val KEYFORDATA = "key"
+    }
+}
